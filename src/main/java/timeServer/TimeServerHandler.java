c0f3f514501f3bcdcd5c2a489054cb127254fb71
william.Liu <893051689@qq.com>
2021-11-09 21:30:09 +0800

    增加时间服务器的相关代码

---
+++ src/main/java/timeServer/TimeServerHandler.java
@@ -0,0 +1,35 @@
+package timeServer;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * 实现TIME协议
+ */
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        final ByteBuf time = ctx.alloc().buffer(4);
+        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+        final ChannelFuture f = ctx.writeAndFlush(time);
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                assert f == channelFuture;
+                ctx.close();
+            }
+        });
+        super.channelActive(ctx);
+    }
+
+}
